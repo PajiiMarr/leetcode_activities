25d843280638d60def126e6930d77da31402216e
PajiiMarr <151644959+PajiiMarr@users.noreply.github.com>
2025-12-01 23:07:25 +0800

    Time Needed to Buy Tickets (Queue)

---
+++ Queues/time_needed_to_buy_tickets.py
@@ -0,0 +1,17 @@
+from traitlets import List
+
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+
+        for i in range(len(tickets)):
+            if i <= k:
+                time += min(tickets[i], tickets[k])
+            else:
+                time += min(tickets[i], tickets[k] - 1)
+
+        return time
+
+sol = Solution()
+print(sol.timeRequiredToBuy([2,3,2], 2))  # Output: 6
